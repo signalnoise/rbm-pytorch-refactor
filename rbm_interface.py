6ec300130d371124df752f2d145290137095e5b1
owen <ofscorey@gmail.com>
2018-02-06 11:41:03 +0000

    first commit

---
+++ rbm_interface.py
@@ -0,0 +1,148 @@
+import argparse
+import torch
+from tqdm import *
+from torch.autograd import Variable
+import rbm_pytorch
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class RBMInterface:
+
+	def __init__(self):
+
+		self.loss_file = None
+		self.progress_bar = None
+
+		self.train_loader = None
+		self.train_op = None
+		self.rbm = None
+
+		self.parser = rbm_parser()
+		self.args = self.parser.parse_args()
+
+		print(self.args)
+		print("Using library:", torch.__file__)
+
+	def train(self):
+
+		iterations_per_epoch = len(self.train_loader)
+
+		for epoch in self.progress_bar:
+
+			loss_ = np.zeros(iterations_per_epoch)
+			full_reconstruction_error = np.zeros(iterations_per_epoch)
+			free_energy_ = np.zeros(iterations_per_epoch)
+
+			for i, (data, target) in enumerate(self.train_loader):
+
+				loss_[i], full_reconstruction_error[i], free_energy_[i] = self.train_step(data)
+
+			re_mean = np.mean(full_reconstruction_error)
+			loss_mean = np.mean(loss_)
+			free_energy_mean = np.mean(free_energy_)
+
+		self.progress_bar.set_description("Epoch %3d - Loss %8.5f - RE %5.3g " % (epoch, loss_mean, re_mean))
+
+		stats = str(epoch) + "\t" + str(loss_mean) + "\t" + str(free_energy_mean) + "\t" + str(re_mean) + "\n"
+		self.loss_file.write(stats)
+
+		if epoch % 10 == 0:
+			torch.save(self.rbm.state_dict(), "trained_rbm.pytorch." + str(epoch))
+
+		# Save the final model
+		torch.save(self.rbm.state_dict(), "trained_rbm.pytorch." + str(self.args.epochs))
+		self.loss_file.close()
+
+	def train_step(self, data):
+
+		data_input = Variable(data.view(-1, self.args.visible_n))
+
+		new_visible, hidden, h_prob, v_prob = self.rbm(data_input)
+
+		data_free_energy = self.rbm.free_energy(data_input)
+		loss = data_free_energy - self.rbm.free_energy(new_visible)
+
+		reconstruction_error = self.rbm.loss(data_input, new_visible)
+
+		# Update gradients
+		self.train_op.zero_grad()
+		# manually update the gradients, do not use autograd
+		self.rbm.backward(data_input, new_visible)
+		self.train_op.step()
+
+		# Returning .data[0] converts from autograd.Variable to float
+		return loss.data[0], reconstruction_error.data[0], data_free_energy.data[0]
+
+	def build_model(self, train_loader, optimiser, **kwargs):
+		self.train_loader = train_loader
+
+		self.rbm = rbm_pytorch.RBM(k=self.args.kCD, n_vis=self.args.visible_n, n_hid=self.args.hidden_size)
+
+		if self.args.enable_cuda:
+			self.rbm.cuda()
+
+		if self.args.ckpoint is not None:
+			print("Loading saved network state from file", self.args.ckpoint)
+			self.rbm.load_state_dict(torch.load(self.args.ckpoint))
+
+		self.train_op = optimiser(self.rbm.parameters(), **kwargs)
+
+	def initialise_output(self):
+		self.progress_bar = tqdm(range(self.args.start_epoch, self.args.epochs))
+
+		filename = self.args.text_output_dir + "Loss_timeline_lr" + str(self.lr) + "_wd" + str(self.wd) + "_mom"\
+		           + str(self.mom) + ".data"
+
+		header = "#Epoch \t  Loss mean \t free energy mean \t reconstruction error mean \n"
+
+		self.loss_file = open(filename, "w", buffering=1)
+		self.loss_file.write(header)
+
+	def get_args(self):
+
+		return self.args
+
+class rbm_parser(argparse.ArgumentParser):
+
+	def __init__(self):
+		super().__init__(description='Process arguments for a Restricted Boltzmann Machine')
+
+		self.add_argument('--ckpoint', dest='ckpoint', help='Path to RBM saved state',
+		                         type=str)
+		self.add_argument('--start', dest='start_epoch', default=0, help='Choose the first epoch number',
+		                         type=int)
+		self.add_argument('--epochs', dest='epochs', default=10, help='number of epochs',
+		                         type=int)
+		self.add_argument('--batch', dest='batches', default=100, help='batch size',
+		                         type=int)
+		self.add_argument('--hidden', dest='hidden_size', default=64, help='Number of hiddne nodes',
+		                         type=int)
+		self.add_argument('--visible', dest='visible_n', default=64, help='Number of visible nodes')
+
+		self.add_argument('--k', dest='kCD', default=2, help='number of Contrastive Divergence steps',
+		                         type=int)
+		self.add_argument('--training_data', dest='training_data', default='../../state0t2.1.txt',
+		                         help='path to training input data',
+		                         type=str)
+		self.add_argument('--txtout', dest='text_output_dir', default='./',
+		                         help='Directory in which to save text output data',
+		                         type=str)
+		self.add_argument('--enable_cuda', dest='enable_cuda', default=False, help='Specifies if cuda will be used')
+
+class CSV_Ising_dataset(Dataset):
+    """ Defines a CSV reader """
+    def __init__(self, csv_file, size=32, transform=None):
+        self.csv_file = csv_file
+        self.size = size
+        csvdata = np.loadtxt(csv_file, delimiter=",", skiprows=1, dtype="float32")
+        self.imgs = torch.from_numpy(csvdata.reshape(-1, size))
+        self.datasize, sizesq = self.imgs.shape
+        self.transform = transform
+        print("Loaded training set of %d states" % self.datasize)
+
+    def __getitem__(self, index):
+        return self.imgs[index], index
+
+    def __len__(self):
+        return len(self.imgs)
---
+++ rbm_pytorch.py
@@ -0,0 +1,184 @@
+###############################################################
+#
+# Restricted Binary Boltzmann machine in Pytorch
+# Possible input sets: MNIST, ISING model configurations
+#
+# RBM module
+#
+# 2017 Guido Cossu <gcossu.work@gmail.com>
+#
+##############################################################
+
+
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tqdm import *
+
+import argparse
+import torch
+import torch.utils.data
+#import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid, save_image
+from math import exp
+
+
+class CSV_Ising_dataset(Dataset):
+    """ Defines a CSV reader """
+    def __init__(self, csv_file, size=32, transform=None):
+        self.csv_file = csv_file
+        self.size = size
+        csvdata = np.loadtxt(csv_file, delimiter=",", skiprows=1, dtype="float32")
+        self.imgs = torch.from_numpy(csvdata.reshape(-1, size))
+        self.datasize, sizesq = self.imgs.shape
+        self.transform = transform
+        print("Loaded training set of %d states" % self.datasize)
+
+    def __getitem__(self, index):
+        return self.imgs[index], index
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+class RBM(nn.Module):
+    def __init__(self, n_vis=784, n_hid=500, k=5):
+        super(RBM, self).__init__()
+        # definition of the constructor
+        self.n_vis = n_vis
+        self.n_hid = n_hid
+
+        self.W = nn.Parameter(torch.zeros(n_hid, n_vis))
+        #nn.init.uniform(self.W, a=-1, b=1)
+        nn.init.normal(self.W,mean=0, std=0.01)
+        self.v_bias = nn.Parameter(torch.zeros(n_vis))
+        self.h_bias = nn.Parameter(torch.zeros(n_hid))
+        self.CDiter = k
+
+    def sample_probability(self, prob, random):
+        """Get samples from a tensor of probabilities.
+
+            :param probs: tensor of probabilities
+            :param rand: tensor (of the same shape as probs) of random values
+            :return: binary sample of probabilities
+        """
+        return F.relu(torch.sign(prob - random))
+
+
+    def hidden_from_visible(self, visible):
+        # Enable or disable neurons depending on probabilities
+        probability = torch.sigmoid(F.linear(visible, self.W, self.h_bias))
+        random_field = Variable(torch.rand(probability.size()))
+        new_states = self.sample_probability(probability, random_field)
+        return new_states, probability
+
+    def visible_from_hidden(self, hid):
+        # Enable or disable neurons depending on probabilities
+        probability = torch.sigmoid(F.linear(hid, self.W.t(), self.v_bias))
+        random_field = Variable(torch.rand(probability.size()))
+        new_states = self.sample_probability(probability, random_field)
+
+        return new_states, probability
+
+    def new_state(self, visible, use_prob=False):
+        hidden, probhid = self.hidden_from_visible(visible)
+        if (use_prob):
+            new_visible, probvis = self.visible_from_hidden(probhid)
+        else:
+            new_visible, probvis = self.visible_from_hidden(hidden)
+
+        # new_hidden, probhid_new = self.hidden_from_visible(probvis)
+
+        return hidden, probhid, new_visible, probvis
+
+    def forward(self, input):
+        """
+        Necessary function for Module classes
+        In the forward function we accept a Variable of input data and we must return
+        a Variable of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Variables.
+
+        """
+        # Contrastive divergence
+        hidden, h_prob, vis, v_prob = self.new_state(input)
+        
+        for _ in range(self.CDiter-1):
+            hidden, h_prob, vis, v_prob = self.new_state(vis, use_prob=False)
+        
+        return vis, hidden, h_prob, v_prob
+
+    def loss(self, ref, test):
+        return F.mse_loss(test, ref, size_average=True)
+
+    def spin_flip(self, conf, s = 0):
+        #print("s:",s)
+        if (s > self.n_vis):
+            return
+        if (conf[0,s] == 0):
+            conf[0,s] = 1
+            return
+        else:
+            conf[0,s] = 0
+            self.spin_flip(conf, s+1)
+
+    def probabilities(self, Z):
+        field = torch.zeros(1,self.n_vis)
+        # fill with -1s
+        f_conf = open('probrbm', 'w')
+        for conf in xrange(2**self.n_vis-1):
+            F = self.free_energy(Variable(field)).data.numpy()
+            p = exp(-F)/Z
+            f_conf.write(str(conf)+" "+ str(p) + " " + str(Z) + " " + str(F)+"\n")
+            #print(field)
+            self.spin_flip(field) 
+
+        F = self.free_energy(Variable(field)).data.numpy()
+        p = exp(-F)/Z
+        f_conf.write(str(conf+1)+" "+ str(p) + " " + str(Z) + " " + str(F)+"\n")
+        f_conf.close()
+
+    def free_energy(self, v):
+        # computes log( p(v) )
+        # eq 2.20 Asja Fischer
+        # double precision
+        vbias_term = v.mv(self.v_bias).double()  # = v*v_bias
+        wx_b = F.linear(v, self.W, self.h_bias).double()  # = vW^T + h_bias
+        
+        # sum over the elements of the vector
+        hidden_term = wx_b.exp().add(1).log().sum(1)  
+
+        # notice that for batches of data the result is still a vector of size num_batches
+        return -(hidden_term + vbias_term).mean()  # mean along the batches
+
+    def backward(self, target, vk):
+        # p(H_i | v) where v is the input data
+        probability = torch.sigmoid(F.linear(target, self.W, self.h_bias))
+
+        # p(H_i | v) where v is the negative visible layer
+        h_prob_negative = torch.sigmoid(F.linear(vk, self.W, self.h_bias))
+        # Update the W
+        training_set_avg = probability.t().mm(target)
+        # The minus sign comes from the implementation of the SGD in pytorch 
+        # see http://www.cs.toronto.edu/~hinton/absps/momentum.pdf
+        # the learning rate has a negative sign in front
+
+        self.W.grad = -(training_set_avg - h_prob_negative.t().mm(vk)) / probability.size(0)
+        #print(self.W.grad)
+
+        # Update the v_bias
+        self.v_bias.grad = -(target - vk).mean(0)
+        #print("vbias", self.v_bias)
+
+        # Update the h_bias
+        self.h_bias.grad = -(probability - h_prob_negative).mean(0)
+        #print("hbias grad", self.h_bias.grad)
+
